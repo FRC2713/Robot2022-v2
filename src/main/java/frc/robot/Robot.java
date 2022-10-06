eea8be8f7b8ddec3b0745b11b8b861a1a6702545
Justin <justin@tervay.com>
2022-10-05 22:39:23 -0400

    Remove unnecessary cruft, add some buttons to move the modules

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,27 +4,21 @@
 
 package frc.robot;
 
-import org.littletonrobotics.junction.LoggedRobot;
-
-import edu.wpi.first.cameraserver.CameraServer;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.commands.DefaultDrive;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.subsystems.BabySwerver;
 import frc.robot.util.characterization.CharacterizationCommand;
 import frc.robot.util.characterization.CharacterizationCommand.FeedForwardCharacterizationData;
+import org.littletonrobotics.junction.LoggedRobot;
 
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
 public class Robot extends LoggedRobot {
   public static final BabySwerver swerveDrive = new BabySwerver();
 
   public static final XboxController driver = new XboxController(Constants.zero);
-  public static final XboxController operator = new XboxController(1);
 
   public static final FeedForwardCharacterizationData ffData =
       new FeedForwardCharacterizationData("Module Driving");
@@ -38,86 +32,92 @@ public class Robot extends LoggedRobot {
           },
           () -> swerveDrive.getAverageVoltageAppliedForCharacterization());
 
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
   @Override
   public void robotInit() {
-    CameraServer.startAutomaticCapture();
+    // swerveDrive.setDefaultCommand(new DefaultDrive());
 
-    swerveDrive.setDefaultCommand(new DefaultDrive());
+    new JoystickButton(driver, XboxController.Button.kA.value)
+        .whenPressed(
+            new InstantCommand(
+                () -> {
+                  swerveDrive.setModuleStates(
+                      new SwerveModuleState[] {
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(0)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(0)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(0)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(0)),
+                      });
+                },
+                swerveDrive));
+    new JoystickButton(driver, XboxController.Button.kB.value)
+        .whenPressed(
+            new InstantCommand(
+                () -> {
+                  swerveDrive.setModuleStates(
+                      new SwerveModuleState[] {
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(90)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(90)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(90)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(90)),
+                      });
+                },
+                swerveDrive));
+    new JoystickButton(driver, XboxController.Button.kY.value)
+        .whenPressed(
+            new InstantCommand(
+                () -> {
+                  swerveDrive.setModuleStates(
+                      new SwerveModuleState[] {
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(180)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(180)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(180)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(180)),
+                      });
+                },
+                swerveDrive));
+    new JoystickButton(driver, XboxController.Button.kX.value)
+        .whenPressed(
+            new InstantCommand(
+                () -> {
+                  swerveDrive.setModuleStates(
+                      new SwerveModuleState[] {
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(270)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(270)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(270)),
+                        new SwerveModuleState(0, Rotation2d.fromDegrees(270)),
+                      });
+                },
+                swerveDrive));
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
-    // block in order for anything in the Command-based framework to work.
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
-  public void disabledInit() {
-    // if (DriverStation.isFMSAttached()) {
-    // RobotContainer.limelight.setLedMode(LedMode.FORCE_ON);
-    // } else {
-    // RobotContainer.limelight.setLedMode(LedMode.FORCE_OFF);
-    // }
-  }
+  public void disabledInit() {}
 
   @Override
   public void disabledPeriodic() {}
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {}
 
-  /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {}
 
   @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-  }
+  public void teleopInit() {}
 
-  /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {
-    // hello!
-    // if (RobotContainer.snekSystem.getUpperLimit()) {
-    // RobotContainer.driver.setRumble(RumbleType.kLeftRumble, 0.5);
-    // RobotContainer.driver.setRumble(RumbleType.kRightRumble, 0.5);
-    // } else {
-
-    // RobotContainer.driver.setRumble(RumbleType.kLeftRumble, 0);
-    // RobotContainer.driver.setRumble(RumbleType.kRightRumble, 0);
-    // }
-  }
+  public void teleopPeriodic() {}
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 
