abb2570656a20a2b5fc64eab36f289d5b589799c
The Fake Slim Shady <74916637+Sid220@users.noreply.github.com>
2022-12-10 15:46:34 -0500

    Improve error handling for errorHandleSparkMAX + documentation

--- src/main/java/frc/robot/util/RedHawkUtil.java
+++ src/main/java/frc/robot/util/RedHawkUtil.java
@@ -7,9 +7,22 @@ import org.littletonrobotics.junction.Logger;
 
 @UtilityClass
 public final class RedHawkUtil {
+  /**
+   * Checks whether the given REVLibError is actually an error, and then logs it to AdvantageScope
+   * and SmartDasboard. SmartDashboard variable logged is "RevLibError" and "RevLibError/name"
+   * AdvantageScope variable logged is "RevLibError/name"
+   *
+   * @param status A RevLibError
+   * @param name The name of the RevLibError, logged (see description)
+   */
   public static void errorHandleSparkMAX(REVLibError status, String name) {
+    String stackTrace = "";
+    StackTraceElement[] rawStackTrace = Thread.currentThread().getStackTrace();
     if (status != REVLibError.kOk) {
-      Logger.getInstance().recordOutput("RevLibError/" + name, status.name());
+      for (int i = 0; i < rawStackTrace.length; i++) {
+        stackTrace.concat(rawStackTrace[i].toString() + " ");
+      }
+      Logger.getInstance().recordOutput("RevLibError/" + name, status.name() + " StackTrace: ");
       SmartDashboard.putBoolean("RevLibError/" + name, false);
       SmartDashboard.putBoolean("RevLibError", false);
     }
