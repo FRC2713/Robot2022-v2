1b4e43ad4cd59265f8608ce5d6824d4fb83e50b6
The Fake Slim Shady <74916637+Sid220@users.noreply.github.com>
2022-12-17 09:54:27 -0500

    Complete ErrorHandling!

--- src/main/java/frc/robot/util/RedHawkUtil.java
+++ src/main/java/frc/robot/util/RedHawkUtil.java
@@ -2,6 +2,8 @@ package frc.robot.util;
 
 import com.revrobotics.REVLibError;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.NonNull;
 import lombok.experimental.UtilityClass;
 import org.littletonrobotics.junction.Logger;
@@ -17,15 +19,50 @@ public final class RedHawkUtil {
    * @param name The name of the RevLibError, logged (see description)
    */
   public static void errorHandleSparkMAX(@NonNull REVLibError status, @NonNull String name) {
-    String stackTrace = "";
-    StackTraceElement[] rawStackTrace = Thread.currentThread().getStackTrace();
     if (status != REVLibError.kOk) {
+      StackTraceElement[] rawStackTrace = Thread.currentThread().getStackTrace();
+      String stackTrace = "";
       for (int i = 0; i < rawStackTrace.length; i++) {
         stackTrace.concat(rawStackTrace[i].toString() + " ");
       }
-      Logger.getInstance().recordOutput("RevLibError/" + name, status.name() + " StackTrace: ");
+      Logger.getInstance()
+          .recordOutput("RevLibError/" + name, status.name() + " StackTrace: " + stackTrace);
       SmartDashboard.putBoolean("RevLibError/" + name, false);
       SmartDashboard.putBoolean("RevLibError", false);
+      ErrHandler.getInstance().addError(status.name() + " StackTrace: " + stackTrace);
+    }
+  }
+
+  public static class ErrHandler {
+    private static ErrHandler INSTANCE;
+
+     /**
+     * Gets the instance of the ErrHandler singleton
+     *
+     * @return The one instance of ErrHandler
+     */
+    public static ErrHandler getInstance() {
+      if (INSTANCE == null) {
+        INSTANCE = new ErrHandler();
+      }
+      return INSTANCE;
+    }
+
+    private ErrHandler() {}
+
+    private List<String> errors = new ArrayList<>();
+
+    /**
+    * Adds an error to the list of RevLib errors.
+    * Must be called on the singleton (see getInstance)
+    */
+    public void addError(@NonNull String error) {
+      this.errors.add(error);
+      this.log();
+    }
+
+    private void log() {
+      Logger.getInstance().recordOutput("RevLibErrors", String.join("\n", errors));
     }
   }
 }
